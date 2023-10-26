9e8cfc81b06028ea4591a0e6299871ca88411f5d
dhillenbp179 <dhillen.mutu@gmail.com>
2023-10-26 07:01:36 +0700

    create api update profile

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const usersHandler = require("./handler/users");
 
-/* GET users listing. */
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
+router.put("/:id", usersHandler.update);
 
 module.exports = router;
